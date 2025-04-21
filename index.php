7b177dee52baa0c1bff6599e835c5f541f8531fa
vaiperlexmar <starodubcev.ilya@myrubikon.tech>
2025-04-21 12:44:29 +0300

    feat: Добавлена главная страница
    - добавлена главная страница
    - рефакторинг логики отрисовки новостей: выделена отдельная функция под создание карточек
    - добавлены стили под десктоп
    - рефакторинг стилей формы

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+
+require_once __DIR__ . '/includes/db/db.php';
+require_once __DIR__ . '/includes/functions.php';
+
+$DB = new \DB\db\DB();
+
+$news = $DB->select("SELECT * FROM news ORDER BY created_at DESC LIMIT 3");
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 
@@ -7,5 +17,27 @@
     <title>Главная | Demis_Test</title>
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
+<header class="header">
+    <a class="link link_bold" href="index.php">
+        <span>Главная</span>
+    </a>
+</header>
+<main>
+    <section class="section news">
+        <h2 class="heading_secondary">Новости</h2>
+        <?php
+        foreach ($news as $news_item) {
+            echo createNewsCard($news_item);
+        }
+        ?>
 
+        <a class="link link_red-block link_bold" href="news.php">
+            Все новости
+        </a>
+    </section>
+    <section class="section feedback">
+        <h2 class="heading_secondary">Оставить обратную связь</h2>
+        <a class="link link_red-block link_bold" href="feedback_form.html">Обратная связь</a>
+    </section>
+</main>
 </html>
