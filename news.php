7d651283a33f07bf1f143025576b0eb4cf7139ee
vaiperlexmar <starodubcev.ilya@myrubikon.tech>
2025-04-21 13:01:01 +0300

    bugfix: Небольшие исправления
    - исправлен вывод ошибок в script.js
    - исправлена опечатка в элементе ошибки email
    - functions переименованы в helpers, потому что я так захотел

--- news.php
+++ news.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . '/includes/db/db.php';
-require_once __DIR__ . '/includes/functions.php';
+require_once __DIR__ . '/includes/helpers.php';
 
 $DB = new \DB\db\DB();
 
